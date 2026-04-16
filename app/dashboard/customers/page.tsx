4e1c18ca05a75fa357165595d93bd362002ac520
tareqSabra <tareqjammal98@gmail.com>
2026-04-16 16:57:24 +0300

    fix(dashboard): update Page component to handle optional searchParams and improve suspense fallback

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -4,12 +4,13 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { query?: string };
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
 }) {
-  const query = searchParams.query || '';
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
 
   return (
     <div className="w-full">
@@ -17,7 +18,9 @@ export default async function Page({
         <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search customers..." />
+        <Suspense fallback={<div className="w-full" />}>
+          <Search placeholder="Search customers..." />
+        </Suspense>
       </div>
       <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
         <CustomersTable query={query} />
